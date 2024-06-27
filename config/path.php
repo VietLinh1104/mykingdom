f7e8a6a501f07cafc8162c7902dadc9b20410053
VietLinh1104 <152302453+VietLinh1104@users.noreply.github.com>
2024-06-28 02:11:14 +0700

    Update path.php

--- config/path.php
+++ config/path.php
@@ -1,9 +1,9 @@
 <?php
     define('ROOT_PATH', $_SERVER['DOCUMENT_ROOT']); // 
-    define('PROCESSING_PATH', '/toy/app/processing/'); // 
-    define('CORE_PATH', '/toy/app/core/'); // 
-    define('PAGE_PATH', '/toy/app/page/'); // 
-    define('UPLOAD_PATH', '/toy/app/processing/upload/'); // 
-    define('ELEMENTS_PATH', '/toy/app/elements/'); // 
-    define('MEDIA_PATH', '/toy/app/media/'); // 
+    define('PROCESSING_PATH', '/app/processing/'); // 
+    define('CORE_PATH', '/app/core/'); // 
+    define('PAGE_PATH', '/app/page/'); // 
+    define('UPLOAD_PATH', '/app/processing/upload/'); // 
+    define('ELEMENTS_PATH', '/app/elements/'); // 
+    define('MEDIA_PATH', '/app/media/'); // 
 ?>
